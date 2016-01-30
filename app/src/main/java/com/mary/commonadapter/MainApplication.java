c687f18a30d3dbcbdb2176e60ae44f9122556362
Mary98 <Mary_zi@outlook.com>
2016-01-30 21:43:07 +0800

    show image

---
+++ app/src/main/java/com/mary/commonadapter/MainApplication.java
@@ -0,0 +1,13 @@
+package com.mary.commonadapter;
+
+import android.app.Application;
+
+/**
+ * Created by Mary on 16/1/30.
+ */
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
